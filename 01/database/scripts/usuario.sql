ea2dc0529a91d640ca532861f8118e3cbec34b5a
elliotgaramendi <elliotgaramendi@gmail.com>
2024-09-12 11:52:44 -0500

    feat: Update 01-DB Initial scripts

--- 01/database/scripts/usuario.sql
+++ 01/database/scripts/usuario.sql
@@ -1,14 +1,16 @@
 alter session set "_ORACLE_SCRIPT" = true;
 
+drop user user01 cascade;
+
 create user user01 identified by tecsup
-	default tablespace users
-	temporary tablespace temp;
+  default tablespace users
+  temporary tablespace temp;
 
 grant
-	create session
+  create session
 to user01;
 grant connect to user01;
 grant resource to user01;
 grant
-	unlimited tablespace
+  unlimited tablespace
 to user01;
