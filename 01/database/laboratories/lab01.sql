1dfabf2e16249a703e9a5dbfe85901d187aa057d
elliotgaramendi <elliotgaramendi@gmail.com>
2024-09-12 20:40:50 -0500

    refactor: Update 01-DB Lab 1 statements

--- 01/database/laboratories/lab01.sql
+++ 01/database/laboratories/lab01.sql
@@ -1,4 +1,5 @@
--- 1. Mostrar el nombre, salario y cod_dept con el alias "departamento", de los empleados de los departamentos 10 y 30, además ordenados por el máximo salario.
+-- 1. Mostrar el nombre, salario y cod_dept con el alias "departamento",
+--    de los empleados de los departamentos 10 y 30, además ordenados por el máximo salario.
 select nombre_emp,
        salario,
        cod_dept as "departamento"
@@ -7,7 +8,8 @@ select nombre_emp,
                      30 )
  order by salario desc;
 
--- 2. Mostrar a los empleados que tienen el tercer carácter del nombre la letra “R” y terminan con “Z” o con “S”.
+-- 2. Mostrar a los empleados que tienen el tercer carácter del nombre
+--    la letra “R” y terminan con “Z” o con “S”.
 select nombre_emp
   from empleados
  where substr(
@@ -18,7 +20,8 @@ select nombre_emp
    and ( nombre_emp like '%Z'
     or nombre_emp like '%S' );
 
--- 3. Mostrar los empleados que ingresaron el año 81 u 82 en los meses de enero o diciembre. No deben ser gerentes y deben tener un salario entre 1000 y 3500.
+-- 3. Mostrar los empleados que ingresaron el año 81 u 82 en los meses de enero o diciembre.
+--    No deben ser gerentes y deben tener un salario entre 1000 y 3500.
 select nombre_emp,
        puesto,
        salario,
@@ -31,7 +34,8 @@ select nombre_emp,
    and puesto <> 'GERENTE'
    and salario between 1000 and 3500;
 
--- 4. Mostrar los empleados de los departamentos 10 y 30 que tengan derecho a comisión y que su puesto sea ANALISTA, VENDEDOR o ADMINISTRATIVO.
+-- 4. Mostrar los empleados de los departamentos 10 y 30 que tengan derecho a comisión
+--    y que su puesto sea ANALISTA, VENDEDOR o ADMINISTRATIVO.
 select nombre_emp,
        puesto,
        comision,
@@ -44,7 +48,8 @@ select nombre_emp,
                    'VENDEDOR',
                    'ADMINISTRATIVO' );
 
--- 5. Mostrar los inventos de EDISON, LAND, LOUD y PASCAL que hayan realizado en la década de los 40, 70 y 80; ordenados por año y por inventor.
+-- 5. Mostrar los inventos de EDISON, LAND, LOUD y PASCAL que hayan realizado en
+--    la década de los 40, 70 y 80; ordenados por año y por inventor.
 select invention,
        inventor,
        year
@@ -59,7 +64,8 @@ select invention,
  order by year,
           inventor;
 
--- 6. Mostrar el Código de nación, Nombre de nación, Capital, Área y Población (usar estos alias) de las naciones que inicien con I o G y que tengan un área mayor a 100,000.
+-- 6. Mostrar el Código de nación, Nombre de nación, Capital, Área y Población (usar estos alias)
+--    de las naciones que inicien con I o G y que tengan un área mayor a 100,000.
 select code as "Código de nación",
        nation as "Nombre de nación",
        capital as "Capital",
@@ -71,14 +77,15 @@ select code as "Código de nación",
    and area > 100000;
 
 -- 7. Escribir la siguiente consulta y analizar el resultado:
--- i. SELECT NOMBRE_EMP, PUESTO, SALARIO FROM EMPLEADOS
--- ii. WHERE SALARIO >= 1500
+-- i.   SELECT NOMBRE_EMP, PUESTO, SALARIO FROM EMPLEADOS
+-- ii.  WHERE SALARIO >= 1500
 -- iii. AND PUESTO = 'GERENTE' OR PUESTO = 'VENDEDOR';
 
 -- Análisis: La consulta selecciona el nombre, puesto y salario de los empleados que cumplen cualquiera de las siguientes condiciones:
---   Tienen un salario mayor o igual a 1500 y su puesto es 'GERENTE'.
---   Su puesto es 'VENDEDOR', sin importar el salario.
--- Esto se debe a que el operador OR tiene menor precedencia que el AND, por lo que la consulta evalúa primero la condición AND y luego la condición OR se aplica a todos los resultados.
+--          Tienen un salario mayor o igual a 1500 y su puesto es 'GERENTE'.
+--          Su puesto es 'VENDEDOR', sin importar el salario.
+-- Esto se debe a que el operador OR tiene menor precedencia que el AND,
+-- por lo que la consulta evalúa primero la condición AND y luego la condición OR se aplica a todos los resultados.
 select nombre_emp,
        puesto,
        salario
@@ -88,11 +95,13 @@ select nombre_emp,
     or puesto = 'VENDEDOR';
 
 -- 8. Escribir la siguiente consulta y compararla con la anterior.
--- i. SELECT NOMBRE_EMP, PUESTO, SALARIO FROM EMPLEADOS
--- ii. WHERE SALARIO >= 1500
+-- i.   SELECT NOMBRE_EMP, PUESTO, SALARIO FROM EMPLEADOS
+-- ii.  WHERE SALARIO >= 1500
 -- iii. AND (PUESTO = 'GERENTE' OR PUESTO = 'VENDEDOR');
 
--- Análisis: La consulta selecciona el nombre, puesto y salario de los empleados cuyo salario es mayor o igual a 1500 y cuyo puesto es 'GERENTE' o 'VENDEDOR'. Los paréntesis aseguran que ambos puestos se evalúen con el salario >= 1500, aplicando AND después de OR.
+-- Análisis: La consulta selecciona el nombre, puesto y salario de los empleados cuyo salario es mayor o igual a 1500
+--          y cuyo puesto es 'GERENTE' o 'VENDEDOR'. Los paréntesis aseguran que ambos puestos se evalúen con el salario >= 1500,
+--          aplicando AND después de OR.
 select nombre_emp,
        puesto,
        salario
@@ -101,7 +110,8 @@ select nombre_emp,
    and ( puesto = 'GERENTE'
     or puesto = 'VENDEDOR' );
 
--- 9. Mostrar los nombres, puestos y salarios de aquellos empleados que laboren en el departamento 30. Ordenarlos en función al puesto que ocupen, al salario (de mayor a menor) y finalmente por nombre alfabéticamente.
+-- 9. Mostrar los nombres, puestos y salarios de aquellos empleados que laboren en el departamento 30.
+--    Ordenarlos en función al puesto que ocupen, al salario (de mayor a menor) y finalmente por nombre alfabéticamente.
 select nombre_emp,
        puesto,
        salario
@@ -111,7 +121,8 @@ select nombre_emp,
           salario desc,
           nombre_emp;
 
--- 10. Mostrar a todos los departamentos almacenados en la tabla DEPART ordenados en forma descendente por ubicación y por código. Emplear posiciones para el ordenamiento.
+-- 10. Mostrar a todos los departamentos almacenados en la tabla DEPART
+--     ordenados en forma descendente por ubicación y por código. Emplear posiciones para el ordenamiento.
 select cod_dept,
        nombre_dept,
        ubicacion
