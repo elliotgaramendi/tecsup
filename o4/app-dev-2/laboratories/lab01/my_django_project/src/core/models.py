8b274f9148ac7a9d8e554fdc498cbf361edd9574
elliotgaramendi <elliotgaramendi@gmail.com>
2025-03-19 14:33:19 -0500

    feat: Add Item model (o4 app-dev-2 L1)

--- o4/app-dev-2/laboratories/lab01/my_django_project/src/core/models.py
+++ o4/app-dev-2/laboratories/lab01/my_django_project/src/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
