e02e1a6ae28d6a5104b7086fc74f71479818ca8e
elliotgaramendi <elliotgaramendi@gmail.com>
2025-03-19 14:36:44 -0500

    feat: Add item list view (o4 app-dev-2 L1)

--- o4/app-dev-2/laboratories/lab01/my_django_project/src/core/views.py
+++ o4/app-dev-2/laboratories/lab01/my_django_project/src/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'core/item_list.html', {'items': items})
