2aadbafabd1ecc9d9af6ea2681877c8e5f0a428f
elliotgaramendi <elliotgaramendi@gmail.com>
2025-03-26 16:46:36 -0500

    feat: Implement task management views (o4 app-dev-2 L2)

--- o4/app-dev-2/laboratories/lab02/django_task_manager/src/tasks/views.py
+++ o4/app-dev-2/laboratories/lab02/django_task_manager/src/tasks/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+
+    status_filter = request.GET.get('status')
+    priority_filter = request.GET.get('priority')
+
+    if status_filter and status_filter != 'all':
+        tasks = tasks.filter(status=status_filter)
+
+    if priority_filter and priority_filter != 'all':
+        tasks = tasks.filter(priority=priority_filter)
+
+    context = {
+        'tasks': tasks,
+        'status_filter': status_filter,
+        'priority_filter': priority_filter,
+    }
+
+    return render(request, 'tasks/task_list.html', context)
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Task created successfully!')
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Task updated successfully!')
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        task.delete()
+        messages.success(request, 'Task deleted successfully!')
+        return redirect('task_list')
+
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
