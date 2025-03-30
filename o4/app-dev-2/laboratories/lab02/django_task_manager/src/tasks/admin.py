fdeef1db9617ea8973b96ba0e58814e9afe5118c
elliotgaramendi <elliotgaramendi@gmail.com>
2025-03-30 18:11:44 -0500

    feat: Register Task model in admin (o4 app-dev-2 L2)

--- o4/app-dev-2/laboratories/lab02/django_task_manager/src/tasks/admin.py
+++ o4/app-dev-2/laboratories/lab02/django_task_manager/src/tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority', 'status', 'due_date', 'created_date')
+    list_filter = ('status', 'priority')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'created_date'
