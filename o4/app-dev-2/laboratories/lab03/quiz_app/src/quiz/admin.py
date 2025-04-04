9ea187af12bda45d6987a78bdd0d1abcb07aac6b
elliotgaramendi <elliotgaramendi@gmail.com>
2025-04-03 22:26:07 -0500

    feat: Enhance admin interface (o4 app-dev-2 L3)

--- o4/app-dev-2/laboratories/lab03/quiz_app/src/quiz/admin.py
+++ o4/app-dev-2/laboratories/lab03/quiz_app/src/quiz/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Exam, Question, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date')
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'exam')
+    inlines = [ChoiceInline]
