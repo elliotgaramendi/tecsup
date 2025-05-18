ed09e347c655561d6042b7711a5b47dea5e0bea5
elliotgaramendi <elliotgaramendi@gmail.com>
2025-05-17 20:28:51 -0500

    fix: Ensure proper formatting in main.jsx (o4 app-dev-2 L9)

--- o4/app-dev-2/laboratories/lab09/sin-e-spoiler/src/main.jsx
+++ o4/app-dev-2/laboratories/lab09/sin-e-spoiler/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './css/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
