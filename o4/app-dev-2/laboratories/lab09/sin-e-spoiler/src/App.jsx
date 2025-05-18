97c56e7fb17658370be44619fe7a20656e5feefb
elliotgaramendi <elliotgaramendi@gmail.com>
2025-05-17 20:28:35 -0500

    feat: Refactor App component to display layout and modules (o4 app-dev-2 L9)

--- o4/app-dev-2/laboratories/lab09/sin-e-spoiler/src/App.jsx
+++ o4/app-dev-2/laboratories/lab09/sin-e-spoiler/src/App.jsx
@@ -1,7 +1,22 @@
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+import Hero from "./components/modules/Hero";
+import MovieList from "./components/modules/MovieList";
+import { getMovies } from "./utils/movie.utils";
+
 function App() {
+  const movies = getMovies();
+
   return (
-    <h1>Sin E Spoiler</h1>
-  )
+    <>
+      <Header />
+      <main className="main">
+        <Hero />
+        <MovieList movies={movies} />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
