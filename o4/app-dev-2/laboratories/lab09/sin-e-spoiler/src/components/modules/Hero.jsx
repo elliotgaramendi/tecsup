fc10f6d49800ca81ca2c9dae2204f647a752c129
elliotgaramendi <elliotgaramendi@gmail.com>
2025-05-17 20:18:49 -0500

    feat: Add Hero component for showcasing movie (o4 app-dev-2 L9)

---
+++ o4/app-dev-2/laboratories/lab09/sin-e-spoiler/src/components/modules/Hero.jsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <article className="hero">
+      <div className="container d-flex f-direction-column g-10">
+        <div className="d-flex f-direction-column a-items-center g-4">
+          <h1 className="hero__title t-align-center">
+            Explore spoiler-free cinema with <span className="c-primary">AI reviews</span>
+          </h1>
+          <p className="hero__paragraph t-align-center">
+            Your movie app with advanced features, premieres, and interactive AR experiences.
+          </p>
+          <a href="#now-showing" className="button button--primary interactive interactive--xl">
+            🎬 Browse Movies
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Hero;
