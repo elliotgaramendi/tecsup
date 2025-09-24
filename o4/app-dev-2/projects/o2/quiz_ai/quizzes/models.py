474cffd39b81914e13b2d691f4521797ebedb65b
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 15:09:44 -0500

    feat: Update quizzes model (o4 app-dev-2 P2)

--- o4/app-dev-2/projects/o2/quiz_ai/quizzes/models.py
+++ o4/app-dev-2/projects/o2/quiz_ai/quizzes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
