2e14f7306258b5d25e6889ce11d6e95d2a007893
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 15:50:15 -0500

    feat: Update quizzes views (o4 app-dev-2 P2)

--- o4/app-dev-2/projects/o2/quiz_ai/quizzes/views.py
+++ o4/app-dev-2/projects/o2/quiz_ai/quizzes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Quiz
+from .serializers import QuizSerializer
 
-# Create your views here.
+
+class QuizViewSet(viewsets.ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
