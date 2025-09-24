9e5013e1705c7285f3989fa772305af2c260e902
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 15:51:50 -0500

    feat: Update quizzes admin (o4 app-dev-2 P2)

--- o4/app-dev-2/projects/o2/quiz_ai/quizzes/admin.py
+++ o4/app-dev-2/projects/o2/quiz_ai/quizzes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Quiz
 
-# Register your models here.
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    search_fields = ['title', 'description']
+    list_filter = ['created_at']
