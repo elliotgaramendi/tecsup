7c5acdd5f13bfc6655004024cb110278a518eb44
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-27 22:06:14 -0500

    feat: Add solutions to topic 1 (o1 programming P1)

---
+++ o1/programming/projects/o1_introduction/main.py
@@ -0,0 +1,57 @@
+# # Simple Addition Calculator ➕
+# print("➕ Calculadora de Suma Simple")
+# print("=" * 30)
+
+# # Your code here 👇
+# number1 = float(input("Número 1: "))
+# number2 = float(input("Número 2: "))
+# result = number1 + number2
+# print(f"El resultado es: {result}")
+
+# # Example output:
+# El resultado es: 15.0
+
+# # Test cases:
+# print("=" * 30)
+# print(result == 15)
+# print(result == 10.0)
+# print(result == 300.0)
+
+# ===
+
+# # Rectangle Area Calculator 📐
+# print("🏗️ Rectangle Area Calculator")
+# print("=" * 40)
+
+# # Your code here 👇
+# base = float(input('Base: '))
+# height = float(input('Altura: '))
+# area = base * height
+# result = f'El área del rectángulo es: {area} metros cuadrados'
+# print(result)
+
+# # Example output:
+# # El área del rectángulo es: 15 metros cuadrados
+
+# print("=" * 40)
+# # Test cases:
+# print(result == 'El área del rectángulo es: 15.0 metros cuadrados')
+# print(result == 'El área del rectángulo es: 24.15 metros cuadrados')
+# print(result == 'El área del rectángulo es: 49.0 metros cuadrados')
+
+# ===
+
+# # Temperature Converter 🌡️
+# print("🌡️ Conversor de Temperatura")
+# print("=" * 30)
+
+# # Your code here 👇
+# celsius = float(input("¿Cuál es la temperatura en grados Celsius? "))
+# fahrenheit = (celsius * 9 / 5) + 32
+# print(f"Fahrenheit es: {fahrenheit:.1f} °F 🌡️🔥")
+
+# # Test cases:
+# print("=" * 30)
+# print(fahrenheit == 32.0)
+# print(fahrenheit == 77.0)
+# print(fahrenheit == 212.0)
