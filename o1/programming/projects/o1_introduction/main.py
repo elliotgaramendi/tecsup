5521328da8b0a0da45dde91e042fcbb100ebfc05
elliotgaramendi <elliotgaramendi@gmail.com>
2025-10-04 14:19:21 -0500

    feat: Update solutions to topic 1 (o1 programming P1)

--- o1/programming/projects/o1_introduction/main.py
+++ o1/programming/projects/o1_introduction/main.py
@@ -1,3 +1,11 @@
+# # ====================================================================
+# # 📚 o1: Algorithms and Code Representation
+# # ====================================================================
+
+# # --------------------------------------------------------------------
+# # o1.1 🧮 Simple Addition Calculator ➕
+# # --------------------------------------------------------------------
+
 # # Simple Addition Calculator ➕
 # print("➕ Calculadora de Suma Simple")
 # print("=" * 30)
@@ -17,7 +25,10 @@
 # print(result == 10.0)
 # print(result == 300.0)
 
-# ===
+
+# # --------------------------------------------------------------------
+# # o1.2 📐 Rectangle Area Calculator
+# # --------------------------------------------------------------------
 
 # # Rectangle Area Calculator 📐
 # print("🏗️ Rectangle Area Calculator")
@@ -39,7 +50,10 @@
 # print(result == 'El área del rectángulo es: 24.15 metros cuadrados')
 # print(result == 'El área del rectángulo es: 49.0 metros cuadrados')
 
-# ===
+
+# # --------------------------------------------------------------------
+# # o1.3 🌡️ Temperature Converter (Celsius to Fahrenheit)
+# # --------------------------------------------------------------------
 
 # # Temperature Converter 🌡️
 # print("🌡️ Conversor de Temperatura")
@@ -55,3 +69,48 @@
 # print(fahrenheit == 32.0)
 # print(fahrenheit == 77.0)
 # print(fahrenheit == 212.0)
+
+
+# # --------------------------------------------------------------------
+# # o1.4 📊 Average Calculator
+# # --------------------------------------------------------------------
+# # Average Calculator 📊
+# print("📊 Calculadora de Promedio")
+# print("=" * 30)
+
+# # Your code here 👇
+# number1 = float(input("Número 1: "))
+# number2 = float(input("Número 2: "))
+# number3 = float(input("Número 3: "))
+
+# average = (number1 + number2 + number3) / 3
+
+# print(f"El promedio es: {average:.2f}")
+
+# # Test cases:
+# print("=" * 30)
+# print(average == 20.0)
+# print(average == 10.0)
+# print(average == 9.0)
+
+
+# # --------------------------------------------------------------------
+# # o1.5 📏 Rectangle Perimeter Calculator
+# # --------------------------------------------------------------------
+# # Rectangle Perimeter Calculator 📏
+# print("📏 Calculadora de Perímetro")
+# print("=" * 30)
+
+# # Your code here 👇
+# base = float(input('Base: '))
+# height = float(input('Altura: '))
+
+# perimeter = 2 * (base + height)
+
+# print(f"El perímetro es: {perimeter}")
+
+# # Test cases:
+# print("=" * 30)
+# print(perimeter == 16)
+# print(perimeter == 34)
+# print(perimeter == 16)
